8263d1bf3b9a3ebaa0eff99a1c0eb0b232e796f4
AnnaStepanian <anna_stepanyan@edu.aua.am>
2023-03-11 22:28:31 +0400

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {AboutUsComponent} from "./about-us/about-us.component";
+import {ServicesComponent} from "./services/services.component";
+import {OurTeamsComponent} from "./our-teams/our-teams.component";
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'our-team', component: OurTeamsComponent }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [
+    RouterModule,
+    MatToolbarModule]
+})
+export class AppRoutingModule { }
+export const RoutingComponent = [HomeComponent, AboutUsComponent, ServicesComponent, OurTeamsComponent]
---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ServicesComponent } from './services/services.component';
+import { OurTeamsComponent } from './our-teams/our-teams.component';
+import {MatIconModule} from "@angular/material/icon";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from "@angular/material/button";
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import {MatInputModule} from "@angular/material/input";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ServicesComponent,
+    OurTeamsComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatIconModule,
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatInputModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
